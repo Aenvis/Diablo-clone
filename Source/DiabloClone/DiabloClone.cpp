6d1969594165dc4150fd9c7a6e6df244c094aefe
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2023-03-19 19:09:54 +0100

    initial commit

---
+++ Source/DiabloClone/DiabloClone.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DiabloClone.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DiabloClone, "DiabloClone" );
---
+++ Source/DiabloClone/DiabloCloneGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DiabloCloneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIABLOCLONE_API ADiabloCloneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
